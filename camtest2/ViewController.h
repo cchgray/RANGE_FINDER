98215673322a7b4a65896072d22528630bc82790
Carter Crenshaw Howard Gray <cchgray@Carters-MacBook-Pro.local>
2014-04-22 08:46:25 -0400

    Initial Commit

---
+++ camtest2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  camtest2
+//
+//  Created by Carter Crenshaw Howard Gray on 4/22/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
